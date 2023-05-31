d2aca798aaf634425b41de18c8982624789bb310
sandeepshinde3108 <sshinde3108@gmail.com>
2023-05-31 10:47:59 +0530

    Jenkins demo project
    Learning jenkins CICD pipeline

---
+++ src/main/java/com/learning/cicd/learningjenkins/LearningJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.cicd.learningjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/cicd/learningjenkins/LearningJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.cicd.learningjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
